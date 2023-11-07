fb1dc4a7890176b21deef32ce0228ae3f69d26ce
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-11-07 21:50:19 +0000

    first sprite hard coded

---
+++ my-app/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card=()=>{
+    return(
+        <>
+            <div className="card">
+                <h2>1</h2>
+                <img src="https://github.com/Kirti-salunkhe/PokemonApp/blob/main/public/images/charmander.png?raw=true" alt="charmander sprite"/>
+                <h2>Charmander</h2>
+            </div>
+        </>
+    )
+}
+export default Card;
